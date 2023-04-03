387172b4fbee6aac3f383c39d66b6ba33a10c7ca
Yeonuk Hwang <younuk23@gmail.com>
2023-04-03 18:09:43 +0900

    FEAT: delete https server open script
    SSL 인증을 EC2에서 처리하지 않고 ALB단으로 마이그레이션 하면서
    https 포트를 따로 열어줄 필요가 없어짐 따라서, https server를 열어주는
    코드 삭제, ssl 디렉토리 삭제, .gitignore에서 ssl 디렉토리 제외

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
-import * as fs from 'fs';
 import * as http from 'http';
-import * as https from 'https';
 import * as express from 'express';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
@@ -26,16 +24,6 @@ async function bootstrap() {
   const port = process.env.PORT || 8000;
   http.createServer(server).listen(port);
   console.info('server is running on ', port);
-
-  const httpsPort = process.env.HTTPS_PORT;
-  if (httpsPort) {
-    const httpsOptions = {
-      ca: fs.readFileSync(__dirname + '/../ssl/ca_bundle.crt'),
-      key: fs.readFileSync(__dirname + '/../ssl/private.key'),
-      cert: fs.readFileSync(__dirname + '/../ssl/certificate.crt'),
-    };
-    https.createServer(httpsOptions, server).listen(httpsPort);
-    console.info('server is running on ', httpsPort);
-  }
 }
+
 bootstrap();
