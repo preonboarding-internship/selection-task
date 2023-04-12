822e48ea5eeb639720439b6f3aac6e04c6682bb8
team7 <hnhyerim@gmail.com>
2023-04-12 20:20:53 +0900

    Env: Create-react-app

--- src/health/health.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TerminusModule } from '@nestjs/terminus';
-import { HealthController } from './health.controller';
-
-@Module({
-  imports: [TerminusModule],
-  controllers: [HealthController],
-})
-export class HealthModule {}
--- src/todos/dto/create-todo.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class CreateTodoDto {
-  @IsNotEmpty()
-  @IsString()
-  todo: string;
-}
