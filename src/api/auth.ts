43a0bceadcc3917a107bd33569c11fa3d1f4e715
team7 <hnhyerim@gmail.com>
2023-04-14 03:58:45 +0900

    Feature: 로그인 API 생성

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,3 +3,6 @@ import { axiosInstance } from "./instance"
 
 /** 회원가입 */
 export const SIGNUP = (data: object) => axiosInstance.post(API_URL.SIGNUP, data)
+
+/** 로그인 */
+export const SIGNIN = (data: object) => axiosInstance.post(API_URL.SIGNIN, data)
