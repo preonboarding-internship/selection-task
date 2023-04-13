bde7841d11017f55dbe9499abf52adbd58ad1f74
team7 <hnhyerim@gmail.com>
2023-04-14 02:35:01 +0900

    Feature: 회원가입 기능 구현 완료

--- src/page/SignUp.tsx
+++ src/page/SignUp.tsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Input from '../component/common/Input'
 import Title from '../component/common/Title'
 import Button from '../component/common/Button'
 import COLORS from '../constant/root'
-import { useNavigate } from 'react-router-dom'
+import { Route, useNavigate } from 'react-router-dom'
 import { ROUTES } from '../constant/routes'
+import { SIGNUP } from '../api/auth'
+import { errorMonitor } from 'events'
 
 const SignUp = () => {
   const navigate = useNavigate()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [disabled, setDisabled] = useState(false)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, type } = e.target
+    switch (type) {
+      case 'email':
+        setEmail(value)
+        break
+      case 'password':
+        setPassword(value)
+        break
+    }
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      setDisabled(true)
+      await SIGNUP({
+        email: email,
+        password: password,
+      })
+      alert('회원가입이 성공하였습니다!')
+      alert('로그인 페이지로 넘어갑니다!')
+      navigate(ROUTES.SIGNIN)
+    } finally {
+      setDisabled(false)
+    }
+  }
 
   return (
     <SignUpStyle>
@@ -17,18 +50,25 @@ const SignUp = () => {
         title='회원가입'
         margin='0 0 50px'
       />
-      <FormStyle>
+      <FormStyle onSubmit={handleSubmit}>
         <Input
           dataTestid='email-input'
+          type='email'
           placeholder='이메일'
+          onChange={handleChange}
+          // onInvalid={}
         />
         <Input
           dataTestid='password-input'
+          type='password'
           placeholder='비밀번호'
+          onChange={handleChange}
         />
         <Button
+          type='submit'
           dataTestid='signup-button'
           text='회원가입'
+          isDisabled={disabled}
         />
       </FormStyle>
       <Button
