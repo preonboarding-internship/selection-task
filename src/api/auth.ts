c03fd2a3818573f978bcc05007b9d81689f77c54
team7 <hnhyerim@gmail.com>
2023-04-14 02:30:26 +0900

    Refactor: 회원가입 api 리팩토링

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,4 +2,4 @@ import API_URL from "../constant/apiUrls"
 import { axiosInstance } from "./instance"
 
 /** 회원가입 */
-export const SIGNUP = async (data: object) => axiosInstance.post(API_URL.SIGNUP, data)
+export const SIGNUP = (data: object) => axiosInstance.post(API_URL.SIGNUP, data)
