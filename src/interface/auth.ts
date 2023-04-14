289ef7a034fb3f8bf69e90de48495cd98a1808d9
team7 <hnhyerim@gmail.com>
2023-04-14 18:55:06 +0900

    Feature: 유효성검사 인터페이스 추가

--- src/interface/auth.ts
+++ src/interface/auth.ts
@@ -2,3 +2,13 @@ export interface IAuth {
   email: string
   password: string
 }
+
+export interface IAuthValid {
+  isEmail: boolean
+  isPassword: boolean
+}
+
+export interface IAuthValidError {
+  emailError: string
+  passwordError: string
+}
