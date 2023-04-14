1d789ba84b48b7c9c1126f11941d97cafe108a6e
team7 <hnhyerim@gmail.com>
2023-04-14 23:38:31 +0900

    Feature: todo

--- src/page/Todo.tsx
+++ src/page/Todo.tsx
@@ -1,32 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Input from '../component/common/Input'
-import TodoItem from '../component/common/TodoItem'
 import Button from '../component/common/Button'
+import { CREATETODO, DELETETODO, GETTODO } from '../api/todo'
+import { FormStyle } from './SignUp'
+import { ICreateTodo, IGetTodo } from '../interface/todo'
+import COLORS from '../constant/root'
+import TodoItem from '../component/common/TodoItem'
 
 const Todo = () => {
+  const [todos, setTodos] = useState<IGetTodo[]>([])
+  const [form, setForm] = useState<ICreateTodo>({
+    id: 1,
+    todo: '',
+    isCompleted: false,
+    userId: 1,
+  })
+  useEffect(() => {
+    ;(async () => {
+      setTodos(await GETTODO())
+    })()
+  }, [])
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      await CREATETODO({
+        id: form.id,
+        todo: form.todo,
+        isCompleted: form.isCompleted,
+        userId: form.userId,
+      })
+    } catch (error) {
+    } finally {
+    }
+  }
+
+  const handleDelete = async (todoId: number) => {
+    await DELETETODO(todoId)
+  }
+  const handleTodoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, isCompleted: e.target.checked })
+  }
+
+  console.log(todos)
   return (
     <TodoStyle>
       <TodoUl>
-        <TodoItem
-          id='todo1'
-          content='Todo 1'
-        />
-        <TodoItem
-          id='todo2'
-          content='Todo 2'
-        />
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id.toString()}
+            id={todo.id.toString()}
+            content={todo.todo}
+            onChange={handleTodoChange}
+            onClick={() => handleDelete(todo.id)}
+          />
+        ))}
       </TodoUl>
       <InputAreaStyle>
-        <Input
-          dataTestid='new-todo-input'
-          placeholder='할 일을 입력하세요.'
-        />
-        <Button
-          buttonType='primaryType'
-          dataTestid='new-todo-add-button'
-          text='추가'
-        />
+        <FormStyle onSubmit={handleSubmit}>
+          <Input
+            dataTestid='new-todo-input'
+            placeholder='할 일을 입력하세요.'
+            onChange={(e) => setForm({ ...form, todo: e.target.value })}
+          />
+          <Button
+            buttonType='primaryType'
+            type='submit'
+            dataTestid='new-todo-add-button'
+            text='추가'
+          />
+        </FormStyle>
       </InputAreaStyle>
     </TodoStyle>
   )
@@ -38,6 +82,20 @@ const TodoStyle = styled.div`
   background-color: aliceblue;
   border-radius: 10px;
   padding: 20px;
+
+  ${FormStyle} {
+    display: flex;
+    flex-direction: row;
+
+    input {
+      padding: 0 20px;
+      font-size: 18px;
+    }
+
+    button {
+      border-radius: 5px;
+    }
+  }
 `
 
 const TodoUl = styled.ul`
@@ -49,14 +107,4 @@ const TodoUl = styled.ul`
 const InputAreaStyle = styled.div`
   display: flex;
   margin-top: 20px;
-
-  input {
-    padding: 0 20px;
-    font-size: 18px;
-  }
-
-  button {
-    margin-left: 3px;
-    border-radius: 5px;
-  }
 `
