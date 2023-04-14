e4c8ca71232e2cfde2d8708c3ebccf9f73e02afa
team7 <hnhyerim@gmail.com>
2023-04-14 23:38:12 +0900

    Fix: GetTodo api 수정

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,11 +1,15 @@
 import API_URL from "../constant/apiUrls";
+import { IGetTodo } from "../interface/todo";
 import { axiosInstance } from "./instance";
 
 /** 투두 생성 */
 export const CREATETODO = (data: object) => axiosInstance.post(API_URL.TODO, data)
 
 /** 투두 가져오기 */
-export const GETTODO = () => axiosInstance.get(API_URL.TODO)
+export const GETTODO = async () => {
+  const data: IGetTodo[] = await axiosInstance.get(API_URL.TODO)
+  return data
+}
 
 /** 투두 업데이트 */
 export const UPDATETODO = (id: number) => axiosInstance.put(API_URL.UPDATETODO(id))
